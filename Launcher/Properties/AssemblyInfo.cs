15ab6877e46be98beebee51b08ebb0643b191c1a
Zack Loveless <bhmath@gmail.com>
2014-08-08 09:57:47 -0700

    Initial commit

---
+++ Launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Launcher")]
+[assembly: AssemblyDescription("A custom launcher for SWTOR to launch it with Steam.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Steam SWTOR Launcher")]
+[assembly: AssemblyCopyright("Copyright (c) 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("25e8d1e4-9024-4848-8321-bd1a41dc6c95")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
